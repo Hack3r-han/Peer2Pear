39e620253e3a2d7aca96e3cba1295eebb81be5ba
Maxuel09 <maxuelmorillocuevas@gmail.com>
2024-01-15 14:04:32 +0100

    Structure of the environment

---
+++ .vite/component/saludo.jsx
@@ -0,0 +1,13 @@
+
+await fetch("https//pokeapi.co/api/v2/pokemon/ditto");
+
+
+
+function Hola(props) {
+    return (
+        <div>
+            <h1>Hola {props.name}</h1>
+        </div>
+    );
+}
+export default Hola
