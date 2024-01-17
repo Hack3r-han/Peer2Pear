3fe8453bc78a8253004d843d4ac589e3823d60b7
laudyeneth <laudyeneth@hotmail.com>
2024-01-17 07:35:22 +0100

    prueba

--- .vite/component/saludo.jsx
+++ .vite/component/saludo.jsx
@@ -1,12 +1,19 @@
+import { useState, useEffect } from 'react';
 
-await fetch("https//pokeapi.co/api/v2/pokemon/ditto");
-
-
+function useFetch(url) {
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    }, []);
+}
 
 function Hola(props) {
     return (
         <div>
             <h1>Hola {props.name}</h1>
+            <button onClick={useFetch("https://pokeapi.co/api/v2/pokemon")}>Fetch</button>
+            <img src={`https://pokeapi.co/api/v2/pokemon/ ${props.name}`} alt="bulasaur" />
         </div>
     );
 }
