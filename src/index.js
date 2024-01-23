5fd446db2e690b01c55f411b10a3cf9403812614
agt1984 <69942981+agt1984@users.noreply.github.com>
2024-01-23 11:41:19 +0100

    compleate actualization of crud

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Navbar, Footer } from './pages/layout'; //importamos tanto el nav bar como el footer de layout.js
 import { Home } from './pages/home';
 import { Products } from './pages/products';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 //se usa est7a funcion para devolver un componente de react
 //la estructura de abajo se encarga de ejecutar los import, ademas, si queremos que las distintas paginas se ejecuten de forma separada y no en la misma pagina se usa el ROUTES
